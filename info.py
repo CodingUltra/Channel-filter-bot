77ab0911dc71dc7666da5c9535203d0bdd3e1f96
CodingUltra <128906681+CodingUltra@users.noreply.github.com>
2023-04-22 17:23:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 29390803
+API_HASH     = "4227e730acf4483f59ad2799c2bd063f"
+BOT_TOKEN    = "5893948581:AAHA804Zp7F-7TRt6dRccUOOq0ulu-bgdeY"
+SESSION      = "BQAzjwGOWIW6Wrp8ZLuJ9bMeVLrQ2flzqE5kSH2aIox9Lai5qGcMS3f3xUUVpWmArdT4K00oaKm6B94umvTb6j-HEBmM41-V30iN4O4oYgWSj-BPfp9ojCby0dYgL6UV2M4AH9Y2sYlL-dJoIUMsHJeS4ERrsyg_u_i9C6Yk1ogp9uKSk8Jij4P01vGdXTnUdlm9QN-n9WG7FmvDt-xUyp9h96igg-r6Agru7hwWU50tSSOP9E6ZlR34NRLCI3elz6ZwWU9-V86_dywYenGp3cX7ohuUDHG9Lq6zkHAbfCN0Z_CfyiqdTsRz6_vmzkUsnf78fBHqXMylnHUcMzezK8UsUnO25wA"
+DATABASE_URI = "mongodb+srv://filedtore_bot:qwertyuiop1234567890@cluster0.znzqs0e.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001935264520
+ADMIN        = 1383315175
+CHANNEL      = "@teamguardianss"
